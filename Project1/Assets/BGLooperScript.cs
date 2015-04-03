9a69bf7652aefc920595b83126267eae2c625118
Austin Carr <austincarr@Austins-MacBook-Pro.local>
2015-04-03 00:36:51 -0700

    Project Submission

---
+++ Project1/Assets/BGLooperScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooperScript : MonoBehaviour {
+
+	int numBGPanels = 6;
+
+	float cloudMax = 4.81f;
+	float cloudMin = 3.12f;
+
+	void Start(){
+		GameObject[] clouds = GameObject.FindGameObjectsWithTag("Cloud");
+
+		foreach (GameObject cloud in clouds) {
+			Vector3 pos = cloud.transform.position;
+			pos.y = Random.Range(cloudMin, cloudMax);
+			cloud.transform.position = pos;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log("Triggered: " + collider.name);
+
+		float widthOfBGObject = ((BoxCollider2D)collider).size.x;
+
+		Vector3 pos = collider.transform.position;
+
+		pos.x += widthOfBGObject * numBGPanels;
+
+		if(collider.tag == "Cloud") {
+			pos.y = Random.Range(cloudMin, cloudMax);
+		}
+		collider.transform.position = pos;
+
+	}
+}
+
+
---
+++ Project1/Assets/CatMovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatMovement : MonoBehaviour {
+
+	Vector3 velocity = Vector3.zero;
+	float flySpeed = 100f;
+	float forwardSpeed = 2f;
+
+	bool didFly = false;
+	bool dead = false;
+
+	public bool godMode = false;
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = transform.GetComponentInChildren<Animator>();
+	}
+
+	// Do Graphic and Input updates here
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+			didFly = true;
+		}
+
+
+	}
+	
+	// Do physics engine updates here
+	void FixedUpdate () {
+
+		if(dead) {
+			return;
+		}
+
+		GetComponent<Rigidbody2D>().AddForce(Vector2.right * forwardSpeed);
+	
+		if (didFly) {
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * flySpeed);
+
+			animator.SetTrigger("Fly");
+
+			didFly = false;
+		}
+		/*if (GetComponent<Rigidbody2D> ().velocity.y > 0)
+			transform.rotation = Quaternion.Euler (0, 0, 0);
+		else {
+			float angle = Mathf.Lerp (0, -90, (-GetComponent<Rigidbody2D> ().velocity.y / 3f));
+			transform.rotation = Quaternion.Euler (0, 0, angle);
+		}*/
+
+	}
+	void OnCollisionEnter2D(Collision2D collision){
+		if(godMode)
+			return;
+
+		animator.SetTrigger("Death");
+		dead = true;
+	}
+
+
+}
