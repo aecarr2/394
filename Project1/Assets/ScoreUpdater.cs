8d209376daa09d862fb62a971033621fbf3833c1
Austin Carr <austincarr@Austins-MacBook-Pro.local>
2015-04-16 20:52:21 -0700

    Newpull

---
+++ Project1/Assets/ScoreUpdater.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreUpdater : MonoBehaviour {
+
+	// This allows the extra collider I added to the cloud prefab to increment the score
+
+	CameraTracksPlayer tracks;
+
+	// Use this for initialization
+	void Start () {
+		// grab the main camera, and get the component from it
+		tracks = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraTracksPlayer>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		// if the player hits the collider, notify the camera's script.
+		if (collider.tag == "Player")
+			tracks.IncrementScore();
+	}
+}
